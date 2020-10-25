750b5269e02d495bb2eeb30a553f25d46472c482
Iogsotot <ajustusa@gmail.com>
2020-10-25 17:33:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,14 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let word = expr.match(/.{1,10}/g);                           //делим на слова по 10
+    let decodeSentence = [];
+    for(let i = 0; i < word.length; i++) {
+        let letter = word[i].replace(/11/g, '-').replace(/10/g, '.').replace(/00/ig,'').replace(/\\*/ig,'');   // сложная операция преобразования всяких там символов в точки и тирешки
+        letter = MORSE_TABLE[letter] ? MORSE_TABLE[letter] : ' ';             // смотрим в нашу мапу и достаём из неё нужные значения ( а если хи нет, то пробел)
+        decodeSentence.push(letter);
+    }
+    return decodeSentence.join('');
 }
 
 module.exports = {
